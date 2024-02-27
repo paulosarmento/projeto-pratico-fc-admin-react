c9d79e376f1da4277692f0ecf14e966646b4d811
Paulo Sarmento <paulo.cesar.sarmento@hotmail.com>
2024-02-27 07:23:56 -0300

    Criando Primeiros componentes reutilizaveis

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
+import { Box, ThemeProvider, createTheme } from "@mui/material";
 import * as React from "react";
-import IconButton from "@mui/material/IconButton";
-import Stack from "@mui/material/Stack";
-import DeleteIcon from "@mui/icons-material/Delete";
-import AlarmIcon from "@mui/icons-material/Alarm";
-import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import Header from "./components/Header";
+import { Layout } from "./components/Layout";
+
+const theme = createTheme({});
 
 function App() {
   return (
-    <Stack direction="row" spacing={1}>
-      <IconButton aria-label="delete">
-        <DeleteIcon />
-      </IconButton>
-      <IconButton aria-label="delete" disabled color="primary">
-        <DeleteIcon />
-      </IconButton>
-      <IconButton color="secondary" aria-label="add an alarm">
-        <AlarmIcon />
-      </IconButton>
-      <IconButton color="primary" aria-label="add to shopping cart">
-        <AddShoppingCartIcon />
-      </IconButton>
-    </Stack>
+    <ThemeProvider theme={theme}>
+      <Box component="main" sx={{ height: "100vh", background: "red" }}>
+        <Header />
+        <Layout>
+          <h1>Hello</h1>
+        </Layout>
+      </Box>
+    </ThemeProvider>
   );
 }
 
