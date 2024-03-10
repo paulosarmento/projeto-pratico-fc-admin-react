75d200b50715029fa0786f91af63b3993b7a2378
Paulo Sarmento <paulo.cesar.sarmento@hotmail.com>
2024-03-10 10:56:00 -0300

    Video Create Form

--- src/features/video/VideoCreate.tsx
+++ src/features/video/VideoCreate.tsx
@@ -1,8 +1,46 @@
 import { Box, Paper, Typography } from "@mui/material";
 import { VideoForm } from "./components/VideoForm";
-import { initialState } from "./videoSlice";
+import {
+  initialState,
+  useCreateVideoMutation,
+  useGetCastMembersQuery,
+  useGetCategoriesQuery,
+  useGetGenresQuery,
+} from "./videoSlice";
+import { useSnackbar } from "notistack";
+import { Video } from "../../types/Videos";
+import { useEffect, useState } from "react";
+import { mapVideoToForm } from "./util";
 
 export const VideoCreate = () => {
+  const { enqueueSnackbar } = useSnackbar();
+
+  const { data: categories } = useGetCategoriesQuery({});
+  const { data: genres } = useGetGenresQuery({});
+  const { data: castMembers } = useGetCastMembersQuery({});
+
+  const [createVideo, status] = useCreateVideoMutation();
+  const [videoState, setVideoState] = useState<Video>(initialState);
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setVideoState({ ...videoState, [name]: value });
+  }
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    await createVideo(mapVideoToForm(videoState));
+  }
+
+  useEffect(() => {
+    if (status.isSuccess) {
+      enqueueSnackbar("Video created", { variant: "success" });
+    }
+
+    if (status.error) {
+      enqueueSnackbar("Something went wrong", { variant: "error" });
+    }
+  }, [status, enqueueSnackbar]);
+
   return (
     <Box>
       <Paper>
@@ -12,16 +50,14 @@ export const VideoCreate = () => {
           </Box>
         </Box>
         <VideoForm
-          video={{
-            ...initialState,
-          }}
-          handleChange={() => {}}
-          handleSubmit={() => {}}
-          genres={[]}
-          categories={[]}
-          castMembers={[]}
-          isDisabled={false}
-          isLoading={false}
+          video={videoState}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+          genres={genres?.data}
+          categories={categories?.data}
+          castMembers={castMembers?.data}
+          isDisabled={status.isLoading}
+          isLoading={status.isLoading}
         />
       </Paper>
     </Box>
